42f59d3d6d5da6d09127e7cbf73122bfc102e4ee
tusharthexD <80884678+tusharthexD@users.noreply.github.com>
2023-07-02 16:22:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+
+function roller(){
+var dice = ["./images/dice1.png",
+"./images/dice1.png",
+"./images/dice2.png",
+"./images/dice3.png",
+"./images/dice4.png",
+"./images/dice4.png",
+"./images/dice5.png",
+"./images/dice6.png"]; 
+
+var diceroll = Math.floor(Math.random()*6)+1;
+var diceroll2 = Math.floor(Math.random()*6)+1;
+img = document.getElementById("first")
+img.src = dice[diceroll];
+img = document.getElementById("second")
+img.src = dice[diceroll2];
+if (diceroll > diceroll2){
+    document.querySelector(".player1").src = "./images/flag.png";
+    document.querySelector(".player2").src = "";
+}
+else if(diceroll == diceroll2){
+    document.querySelector(".player1").src = "./images/flag.png";
+    document.querySelector(".player2").src = "./images/flag.png";
+}
+else{
+    document.querySelector(".player2").src = "./images/flag.png";
+    document.querySelector(".player1").src = "";
+}
+
+
+}
+function addName(){
+var player1 = document.getElementById("mytext").innerText;
+var player2 = document.getElementById("mytext2").innerText;
+    document.querySelector(".dice1").inner = "hello";
+    document.querySelector(".dice2").innerText = "heyy";
+
+}
+function openDiv(){
+    let get = document.querySelector('#div')
+    get.style.display = 'block'
+   }
+function closeDiv(){
+    let get = document.querySelector('#div')
+    get.style.display = 'none'
+   }   
+
+
+
+
+   function setPlayerName () {
+    var playerNameDiv = document.querySelector(".playern1");
+    
+    playerNameDiv.innerHTML = document.getElementById('playerNameInput').value;
+    
+    // hide input field and button
+    document.getElementById('divSetPlayerName').style.display = 'none';
+  }
+  function setPlayerName2 () {
+    var playerNameDiv = document.querySelector(".playern2");
+    
+    playerNameDiv.innerHTML = document.getElementById('playerNameInput2').value;
+    
+    // hide input field and button
+    document.getElementById('divSetPlayerName2').style.display = 'none';
+  }
